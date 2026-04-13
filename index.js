68807bc4a9ba357cfd1dccd780745d22f1c701f3
Danylo Santos <danylo.oficialti@gmail.com>
2026-04-13 14:55:53 -0300

    enable cors

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const { Pool } = require("pg");
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    res.setHeader("Access-Control-Allow-Methods", "*");
+    next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
